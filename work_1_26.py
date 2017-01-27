9644589d7355011c1ca18506bb317bdb2403ab87
Yan Jiang <enaj1125@gmail.com>
2017-01-27 11:02:47 -0800

    Update work_1_26.py

--- work_1_26.py
+++ work_1_26.py
@@ -1,38 +1,48 @@
-import pyspark
+mport pyspark
 import sys
 import json
 from pyspark import SparkConf, SparkContext
 from elasticsearch import Elasticsearch
 
-
 conf = SparkConf().setAppName("YanJ_app").setMaster("spark://ip-172-31-1-12:7077")
 sc = SparkContext(conf = conf)
 
-
+# Input files
 textFile = sc.textFile("hdfs://ip-172-31-1-12:9000/tmp/30.json")
 
+# set elasticsearch
+es = Elasticsearch(['ip-172-31-1-8'], http_auth=('elastic', 'changeme'), verify_certs=False)
 
+
+# Map reduce
 def map_func(line):
     each_line = json.loads(line)
-
+    es = Elasticsearch(['ip-172-31-1-8'], http_auth=('elastic', 'changeme'), verify_certs=False)
+    num += 1
+    print num
     if "created_at" in each_line.keys():
-       #ret = (each_line['id'], each_line['user']['id'], each_line['timestamp_ms'], each_line['text'] )
-        ret = (each_line['user']['id'], each_line['text'] )
+
+        #ret = (each_line['id'], each_line['user']['id'], each_line['timestamp_ms'], each_line['text'] )
+        if 'user' in each_line:
+           # ret = (each_line['user']['id'], each_line['text'] )
+            doc = {'usr_id': each_line['user']['id'], 'ttext': each_line['text']}
+            es.index(index='test_2', doc_type='inputs', body=doc)
     else:
-        ret = (each_line['delete']['status']['id'], '')
-
-    return ret
-
+        #ret = (each_line['delete']['status']['id'], '')
+        if 'user' in each_line:
+            doc = {'usr_id': each_line['user']['id'], 'ttext': ''}
+            es.index(index='test_2', doc_type='inputs', body=doc)
+    #es.index(index='test_2', doc_type='inputs', body=doc)
+    #return ret
 
 def reduce_func(a, b):
     return a + b
 
 
-counts = textFile.map(map_func).reduce(lambda a, b :a+b)
-
-print counts
-#df_read.saveAsTextFile("/tmp/result1")
+counts = textFile.map(map_func)
+counts.saveAsTextFile("/tmp/result")
 print "----------------------------------------------"
+
 
 
 # write into Elasticsearch
