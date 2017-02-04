044f8bf78072e1e7049d917427f202773f51a452
Yan Jiang <enaj1125@gmail.com>
2017-02-04 15:08:52 -0800

    Update elastic_query.sh

--- elastic_query.sh
+++ elastic_query.sh
@@ -1,9 +1,8 @@
-
-curl -u elastic -XGET 'localhost:9200/test_7/_search?pretty' -H 'Content-Type: application/json' -d'
+curl -u elastic -XGET 'localhost:9200/twitter_indice_dl/_search?pretty' -H 'Content-Type: application/json' -d'
 {
   "query": { "range": {
-                "timeStamp": {
-                        "from": "2017/09/02"
+                "ttimes": {
+                        "from": "2015-05-02"
                 }
          }
  }
